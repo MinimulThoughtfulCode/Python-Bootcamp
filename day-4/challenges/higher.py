9b69dee05f771bebe1654f5f683bbd449572ce7c
MinimulThoughtfulCode <pasha.arfan1@gmail.com>
2026-07-30 19:17:09 -0700

    Create higher.py to filter scores above 60
    Add a script to print scores higher than 60.

---
+++ day-4/challenges/higher.py
@@ -0,0 +1,11 @@
+#Give students a list of numbers
+#Write a loop that prints only the scores higher than 60
+
+#Given List of numbers
+numbers = [45, 88, 92, 61, 55]
+
+#Loop through the numbers
+for i in numbers:
+  if i > 60:
+    #Find the numbers greater than 60
+    print(i)
