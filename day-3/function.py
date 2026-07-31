37d1265153b6de7e3b38350104248e40f37eb9b3
MinimulThoughtfulCode <pasha.arfan1@gmail.com>
2026-07-30 17:53:30 -0700

    Implement greet_user and double_number functions
    Added functions to greet a user and double a number.

---
+++ day-3/function.py
@@ -0,0 +1,25 @@
+#Code-a-long Exercise
+
+#1. Greeter
+#Create a function that greets the user
+
+def greet_user(name):
+  #Function greets the user
+  print("Hello " + name + "! Welcome")
+
+#Test the Function
+greet_user("Arfan")
+greet_user("Agastya")
+
+
+#2. Doubles Number
+# Write a function that multiplies any number by 2 and returns it
+
+def double_number(num):
+  #Function doubles the number
+  num = num * 2
+  return num
+
+#Test the Function
+result = double_number(5)
+print(result)
