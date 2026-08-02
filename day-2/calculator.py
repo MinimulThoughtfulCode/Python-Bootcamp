e60dbd4a235043935783edd55f57ae4ceaf05c16
Agastya Sharma <agastya_sharma@icloud.com>
2026-08-02 12:02:56 -0700

    Added first version of calculator.py

---
+++ day-2/calculator.py
@@ -0,0 +1,25 @@
+print("----Calculator----\n")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+operation = input("Enter operation (+, -, *, /): ")
+print("")
+
+if operation == "+":
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif operation == "-":
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operation == "*":
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Error: Division by zero is not allowed.")
+else:
+    print("Error: Invalid operation. Please use +, -, *, or /.")
