6424afb46b5622190f71775a4747278a06539a6a
Aljer Almazan <aljeralmazan28@mittymonarch.com>
2026-08-04 02:09:58 -0700

    Add user input for favorite color and random number

--- day-2/functions.py
+++ day-2/functions.py
@@ -1,3 +1,12 @@
 # input() is designed to ask the user a question, or derive from it an answer
 
 prompt = input("What's your favorite color?")
+num = input("Name a random number")
+print(f"My favorite color is {prompt}, and I like the number {num}.")
+
+# len() is used to count the amount of items in an object
+text = "Hello World!"
+print(len(text))
+
+shopping_list = ["apples", "milk", "bread", "eggs"]
+print(len(shopping_list))
