49289d28affd97dea987b1e135df8a82d23d9d2f
Aljer Almazan <aljeralmazan28@mittymonarch.com>
2026-08-04 11:57:49 -0700

    Update functions.py

--- day-2/functions.py
+++ day-2/functions.py
@@ -1,12 +1,16 @@
 # input() is designed to ask the user a question, or derive from it an answer
 
 prompt = input("What's your favorite color?")
-num = input("Name a random number")
+num = int(input("Name a random number"))
 print(f"My favorite color is {prompt}, and I like the number {num}.")
 
 # len() is used to count the amount of items in an object
 text = "Hello World!"
 print(len(text))
 
+# Output: 12
+
 shopping_list = ["apples", "milk", "bread", "eggs"]
 print(len(shopping_list))
+
+# Output: 4
