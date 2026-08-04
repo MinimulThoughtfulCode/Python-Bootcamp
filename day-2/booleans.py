a458634ada3408e18bb27d58bd1f53787da92791
Aljer Almazan <aljeralmazan28@mittymonarch.com>
2026-08-04 02:05:25 -0700

    Add user login and voting eligibility checks

--- day-2/booleans.py
+++ day-2/booleans.py
@@ -1,1 +1,20 @@
+is_logged_in = True
+has_permission = False
 
+
+if is_logged_in:
+    print("Welcome back to your dashboard!")
+else:
+    print("Please log in first.")
+
+
+
+user_age = 16
+age_limit = 18
+
+can_vote = user_age >= age_limit  
+
+if can_vote:
+    print("You are eligible to vote!")
+else:
+    print("You are not old enough to vote yet.")
